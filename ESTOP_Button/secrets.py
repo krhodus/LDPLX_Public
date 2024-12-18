a0bb5513ea4a59a588c5f94175c0f51113fe6ce5
Kevin Rhodus <95332119+krhodus@users.noreply.github.com>
2024-12-18 10:48:53 -0500

    Create secrets.py

---
+++ ESTOP_Button/secrets.py
@@ -0,0 +1,15 @@
+secrets = {
+    'wifi_ssid': 'SSID',
+    'wifi_password': 'SSID_PSK',
+    'broker' : 'MQTTServerAddress',
+    'mqtt_username' : '',
+    'mqtt_password' : '',
+    'port' : 1883,
+    'client_id' : 'ClientID-MustBeUnique',
+    'subtopic' : 'MQTT Topic for Feedback Parsing (Future)',
+    'HB_topic' : 'MQTT Topic for Heartbeat Notifications',
+    'ESTOP_topic' : 'MQTT Topic for ESTOP Notifications',
+    'Door_topic' : "MQTT Topic for Door Sensor Notifications",
+    'Master_Seq_topic' : 'MQTT Topic for Master FPP Player',
+    'Secondary_Seq_topic' : 'MQTT Topic for Secondary FPP Player'
+    }
